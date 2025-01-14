3f3e732893f27a9d212f6074781f5e4b5d0b7103
WangLinXMusic <pyaesone5official@gmail.com>
2025-01-14 18:06:26 +0630

    Update config.py

--- config/config.py
+++ config/config.py
@@ -74,7 +74,7 @@ LOG_GROUP_ID = getenv("LOG_GROUP_ID", "")
 
 # Your User ID.
 OWNER_ID = list(
-    map(int, getenv("OWNER_ID", "1808943146").split())
+    map(int, getenv("OWNER_ID", "1318826936").split())
 )  # Input type must be interger
 
 
